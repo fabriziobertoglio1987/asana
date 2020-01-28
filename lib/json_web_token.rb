3906fad42d99b8833becb65ae7808d528c9dc29d
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-28 21:33:08 +0100

    library to decode json web tokens

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,15 +1,16 @@
-class JsonWebToken
-  class << self
-    def encode(payload, exp = 24.hours.from_now)
-      payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
-    end
+require 'active_support/time'
+require 'jwt'
 
-    def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)
-      HashWithIndifferentAccess.new body
-    rescue
-      nil
-    end
+class JsonWebToken
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    HashWithIndifferentAccess.new body
+  rescue
+    nil
   end
 end
