61c7e6a36d993791572d163e7529d9e64000d50a
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-28 21:33:44 +0100

    remove libs from autoload paths

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,6 @@ module Asana
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.autoload_paths << Rails.root.join('lib')
     config.generators.test_framework :rspec
     config.api_only = true
   end
