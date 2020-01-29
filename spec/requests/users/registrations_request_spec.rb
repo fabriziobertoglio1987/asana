986fbaa6c3a6b0d0875fa647e1e53223754d83d5
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-29 18:36:01 +0100

    posts /users/registrations test cases

---
+++ spec/requests/users/registrations_request_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Users::Registrations", type: :request do
+  let(:valid_attributes) { { user: FactoryBot.attributes_for(:user) } }
+  let(:invalid_attributes) { { user: { email: '', password: ''} } }
+  let(:user) { double("user") }
+  let(:email) { spy("email") }
+  let(:password) { double("password") }
+  let(:token) { double("token") }
+
+  describe "#create" do
+    context "with valid parameters" do
+      before(:each) do
+        post "/users/registrations", params: valid_attributes
+      end
+
+      it "returns status 200" do
+        expect(response.status).to be 200
+      end
+
+      it "includes the email and token in the body" do
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed_response[:user][:email]).to eql valid_attributes[:user][:email]
+        expect(parsed_response[:user][:token]).to be_present
+      end
+    end
+
+    context "with invalid paramters" do
+      it "renders not_acceptable in the status" do
+        post "/users/registrations", params: invalid_attributes
+        expect(response.status).to be 406
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed_response[:errors].size).to be > 0
+      end
+    end
+  end
+end
