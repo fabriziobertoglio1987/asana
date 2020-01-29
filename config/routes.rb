f62c1c116977559fc7139eb1e57e27623a260b6e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-29 18:37:28 +0100

    registrations/sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :users do 
+    post 'sessions', to: 'sessions#create'
+    post 'registrations', to: 'registrations#create'
+  end
 end
