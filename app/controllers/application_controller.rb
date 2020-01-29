92a8a59c1a1167502b1e95aaeb5a5e36fc9e8d1b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-29 14:19:17 +0100

    moving jwt logic to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  protected
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decode
+    secret = Rails.application.secrets.secret_key_base
+    token = auth_header.split(' ')[1]
+    begin 
+      JWT.decode(token, secret)
+    rescue JWT::DecodeError
+      []
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+require 'jwt'
+
+describe ApplicationController do
+  let(:secret) { double('secret') }
+  let(:encoded_token) { spy('encoded_token') }
+  let(:decoded_token) { double('decoded_token') }
+
+  before do 
+    allow(Rails).to receive_message_chain(:application, :secrets, :secret_key_base).and_return(secret)
+    @request.headers['Authorization'] = encoded_token
+  end
+
+  describe '#decode' do 
+    it 'returns the decoded token' do 
+      allow(JWT::Decode).to receive_message_chain(:new, :decode_segments) { decoded_token }
+      expect(subject.send(:decode)).to be(decoded_token)
+      expect(JWT::Decode).to have_received(:new).with(encoded_token, secret, any_args)
+    end
+
+    it 'returns empty array in case exception is raised' do
+      allow(JWT::Decode).to receive_message_chain(:new, :decode_segments) { raise JWT::DecodeError }
+      expect(subject.send(:decode)).to eql []
+    end
+  end
+end
